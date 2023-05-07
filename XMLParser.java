ca7c8cbceb229e0a1b2347336a0f549645c28bfa
GrandTyrant404 <atsocha@gmail.com>
2023-05-06 22:19:37 -0700

    parse cards doesnt make objects

--- XMLParser.java
+++ XMLParser.java
@@ -75,23 +75,23 @@ public class XMLParser{
 
           // parse positions
           Element partsElem = (Element) elem.getElementsByTagName("parts").item(0);
-          NodeList parts = elem.getElementsByTagName("part");
+          NodeList parts = partsElem.getElementsByTagName("part");
 
           for (int i = 0; i < parts.getLength(); i++) {
             Element partEle = (Element) parts.item(i);
             String partName = partEle.getAttribute("name");
             String partRank = partEle.getAttribute("level");
             Element areaElem2 = (Element) partEle.getElementsByTagName("area").item(0);
+            // future use
             String xPart = areaElem2.getAttribute("x");
             String yPart = areaElem2.getAttribute("y");
             String hPart = areaElem2.getAttribute("h");
             String wPart = areaElem2.getAttribute("w");
-            Node lineNode = partEle .getElementsByTagName("line").item(0);
+            Node lineNode = partEle.getElementsByTagName("line").item(0);
             String partLine = lineNode.getTextContent();
             //test
             System.out.println(partName);
             System.out.println(partLine);
-
           }
         }
       }
@@ -113,6 +113,53 @@ public class XMLParser{
       doc.getDocumentElement().normalize();
       System.out.print("Root element: ");
       System.out.println(doc.getDocumentElement().getNodeName());
+      NodeList list = doc.getElementsByTagName("card");
+
+      for (int temp = 0; temp < list.getLength(); temp++) {
+        Node nNode = list.item(temp);
+
+        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element elem = (Element) nNode;
+          // get set name and info
+          String name = elem.getAttribute("name");
+          String image = elem.getAttribute("img");
+          String budget = elem.getAttribute("budget");
+          //test
+          System.out.println(name + " " + image + " " + budget);
+          System.out.println();
+
+          // get scene number and description
+          Node sceneNode = elem.getElementsByTagName("scene").item(0);
+          String sceneDesc = sceneNode.getTextContent();
+          Element sceneElem = (Element) sceneNode;
+          String sceneNum = sceneElem.getAttribute("number");
+          System.out.println(sceneDesc);
+          System.out.println(sceneNum);
+
+          // get parts
+          NodeList parts = elem.getElementsByTagName("part");
+          for (int i = 0; i < parts.getLength(); i++) {
+            Element partEle = (Element) parts.item(i);
+            String partName = partEle.getAttribute("name");
+            String partRank = partEle.getAttribute("level");
+            Element areaElem2 = (Element) partEle.getElementsByTagName("area").item(0);
+            // future use
+            String xPart = areaElem2.getAttribute("x");
+            String yPart = areaElem2.getAttribute("y");
+            String hPart = areaElem2.getAttribute("h");
+            String wPart = areaElem2.getAttribute("w");
+            Node lineNode = partEle.getElementsByTagName("line").item(0);
+            String partLine = lineNode.getTextContent();
+            //test
+            System.out.println(partName);
+            System.out.println(partLine);
+          }
+        }
+        System.out.println();
+      }
+
+
+
     }
     catch (Exception e) {
        e.printStackTrace();
